c06156666fcd42e9aad8d3092194146c015bad74
Brian Gillingham <bgilling@pitt.edu>
2017-11-13 15:08:51 +0000

    minor adjustment to gauge display

--- theme/islandora-jobs-monitor-gauge.tpl.php
+++ theme/islandora-jobs-monitor-gauge.tpl.php
@@ -7,7 +7,7 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
-          ['Disk Usage', <?php print $disk_used; ?>],
+          ['Disk', <?php print $disk_used; ?>],
           ['CPU', <?php print $cpu; ?>],
           ['Memory', <?php print $memory; ?>]
         ]);
@@ -15,7 +15,9 @@
         var options = {
           width: 220, height: 88,
           redFrom: 90, redTo: 100,
-          yellowFrom:65, yellowTo: 90,
+          yellowFrom: 80, yellowTo: 90,
+          greenColor: '#c9bfa2',
+          greenFrom: 65, greenTo: 80,
           minorTicks: 5
         };
 
