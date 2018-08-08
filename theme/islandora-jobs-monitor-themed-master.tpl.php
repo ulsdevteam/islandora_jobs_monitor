402565d5309c3d8bc205613366ee2c9f2e930cb3
Brian Gillingham <bgilling@pitt.edu>
2018-08-08 16:54:29 -0400

    added display of whether or not gearmand or gearman-workers is running

--- theme/islandora-jobs-monitor-themed-master.tpl.php
+++ theme/islandora-jobs-monitor-themed-master.tpl.php
@@ -1,7 +1,9 @@
-<div class="master" id="<?php print $host_record['hostname']; ?>">
+<div class="master<?php print (($running) ? ' running' : (($host_record['hostname'] <> 'pa-fed-staff-01') ? ' stopped' : '')); ?>" id="<?php print $host_record['hostname']; ?>"
+     <?php if (($host_record['hostname'] <> 'pa-fed-staff-01')) : ?> title="<?php print (($running) ? 'Gearman daemon is running' : 'Gearman daemon is stopped'); ?>"<?php endif; ?>>
   <div class="gauge_wrapper"><?php print $gauge; ?></div>
   <br style="clear:top">
   <?php unset($host_record['hostname']); ?>
+  <?php unset($host_record['running']); ?>
   <?php foreach ($host_record as $key => $value): ?>
     <div class="div_label"><?php print str_replace("_", " ", ucwords($key)); ?></div>
     <div class="div_value"><?php print $value; ?></div>
--- theme/islandora-jobs-monitor-themed-worker.tpl.php
+++ theme/islandora-jobs-monitor-themed-worker.tpl.php
@@ -1,7 +1,8 @@
-<div class="worker" id="<?php print $host_record['hostname']; ?>">
+<div class="worker<?php print (($running) ? ' running' : ' stopped'); ?>" id="<?php print $host_record['hostname']; ?>" title="<?php print (($running) ? 'Gearman workers are running' : 'Gearman workers are stopped'); ?>">
   <div class="gauge_wrapper"><?php print $gauge; ?></div>
   <br style="clear:top">
   <?php unset($host_record['hostname']); ?>
+  <?php unset($host_record['running']); ?>
   <?php foreach ($host_record as $key => $value): ?>
     <div class="div_label"><?php print str_replace("_", " ", ucwords($key)); ?></div>
     <div class="div_value"><?php print $value; ?></div>
