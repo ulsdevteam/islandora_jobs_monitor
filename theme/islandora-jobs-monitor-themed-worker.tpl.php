79e4864506d6105974d4d62032afbe31703ac880
Willow Gillingham <bgilling@pitt.edu>
2019-03-18 15:07:39 -0400

    minor UI improvements - removed the control links from each server monitoring gauge

--- theme/islandora-jobs-monitor-themed-worker.tpl.php
+++ theme/islandora-jobs-monitor-themed-worker.tpl.php
@@ -4,13 +4,19 @@
   <?php unset($host_record['hostname']); ?>
   <?php unset($host_record['running']); ?>
   <?php foreach ($host_record as $key => $value): ?>
+   <?php if ($key <> 'gearman_running'): ?>
     <div class="div_label"><?php print str_replace("_", " ", ucwords($key)); ?></div>
-    <div class="div_value"><?php print $value; ?></div>
+    <div class="div_value">
+<?php print (($key == 'name') ? '<b style="font-size:110%">' : ''); ?>
+     <?php print $value; ?>
+<?php print (($key == 'name') ? '</b>' : ''); ?>
+    </div>
     <br class="clearfix">
+   <?php endif; ?>
   <?php endforeach; ?>
 
   <div class="cpu_graph"><?php print $cpu_values; ?></div>
   <div class="memory_graph"><?php print $memory_values; ?></div>
   
-  <?php print $control_links; ?>
+  <?php /* print $control_links; */ ?>
 </div>
